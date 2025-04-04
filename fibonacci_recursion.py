b62f068a04d25f581b7984712409edf6f4da76bd
ka1sser <serenokaisser.dev@gmail.com>
2025-04-04 08:55:31 +0800

    Edited some print for understanding

--- fibonacci_recursion.py
+++ fibonacci_recursion.py
@@ -2,15 +2,12 @@ def fibonacci(n):
 
     if n == 1:
         print(f"Callstack: fibonacci({n})")
-        print(f"Execution Context: {n}, {n-1}")
+        print(f"Execution Context: fibonacci({n}), fibonacci({n-1})\n")
         return 1
     if n == 0:
         print(f"Callstack: fibonacci({n})")
-        print(f"Execution Context: {n}")
+        print(f"Execution Context: fibonacci({n})\n")
         return 0
     print(f"Callstack: fibonacci({n})")
-    print(f"Execution Context: {n-2}, {n-1}")
+    print(f"Execution Context: fibonacci({n-2}), fibonacci({n-1})\n")
     return fibonacci(n - 2) + fibonacci(n - 1)
-
-
-print(fibonacci(5))
